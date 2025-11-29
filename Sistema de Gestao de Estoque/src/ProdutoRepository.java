501cecac5bff9c5a1567a6048944e124cec8115c
andresilva05 <andresilva9657@gmail.com>
2025-11-29 15:04:02 -0300

    feat: implementar repositório com adicionar, buscarPorId, listar e excluir (soft delete)

--- Sistema de Gestao de Estoque/src/ProdutoRepository.java
+++ Sistema de Gestao de Estoque/src/ProdutoRepository.java
@@ -1,14 +1,17 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProdutoRepository {
+public class ProdutoRepository implements IProdutoRepository {
+
     List<Produto> produtos = new ArrayList<>();
 
+
+    @Override
     public void adicionar(Produto produto) {
         produtos.add(produto);
     }
 
-
+    @Override
     public Produto buscarPorId(int id) {
         for (Produto p : produtos) {
             if (p.getId() == id) {
@@ -18,21 +21,37 @@ public class ProdutoRepository {
         return null;
     }
 
+    @Override
+    public Produto buscarPorNome(String nome) {
+        for (Produto p : produtos) {
+            if (p.getNome().equalsIgnoreCase(nome) && p.isAtivo()) {
+                return p;
+            }
+        }
+        return null;
+    }
 
+    @Override
     public void listar() {
         for (Produto p : produtos) {
             System.out.println(p);
         }
     }
 
+    @Override
     public boolean excluir(int id) {
         Produto produto = buscarPorId(id);
-        if (produto != null) {
-            produto.setAtivo(false); // soft delete
-            return true;
+        if (produto == null) {
+            return false;
         }
-        return false;
+
+        produto.setAtivo(false); // soft delete
+        return true;
     }
 
+    @Override
+    public List<Produto> getAll() {
+        return produtos;
+    }
 
 }
